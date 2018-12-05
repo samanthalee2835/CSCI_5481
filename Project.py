1dbe86b9e7584572078160d24b0fa6c780ced9d4
Benjamin Novacek <novac020@umn.edu>
2018-12-05 15:27:30 -0600

    Now reads in data from csv file

--- Project.py
+++ Project.py
@@ -1,5 +1,21 @@
 # Names: Sam Lee, Noah Kindt, Ben Novacek
 
 import argparse
+import pandas as pd
+
+def make_arg_parser():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-s", "--seqs",
+                      default=argparse.SUPPRESS,
+                      required=True,
+                      help="Path to sequence csv [required]")
+
+  return parser
 
 if __name__ == '__main__':
+  parser = make_arg_parser()
+  args = parser.parse_args()
+
+  seq_file = pd.read_csv(args.seqs)
+
+  print(seq_file)
